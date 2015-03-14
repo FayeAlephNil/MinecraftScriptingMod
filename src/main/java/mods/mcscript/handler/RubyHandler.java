ad38e4bb82649ecc8be7e9bea07f4bee51bc256f
trajing <trajingPlaysGames@gmail.com>
2015-03-14 12:55:42 -0500

    Make RubyHandler

---
+++ src/main/java/mods/mcscript/handler/RubyHandler.java
@@ -0,0 +1,10 @@
+package mods.mcscript.handler;
+
+import cpw.mods.fml.common.eventhandler.Event;
+import mods.mcscript.handler.LangHandler;
+
+public class RubyHandler extends LangHandler{
+    protected void onEvent(Event event, Object code) {
+        //TODO: Everything
+    }
+}
